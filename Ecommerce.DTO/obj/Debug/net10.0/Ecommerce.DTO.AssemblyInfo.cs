db8edafc203337606f374a2f2d1928f877694352
eduardo-marmolejo <eduardomarmolejo.dev00@gmail.com>
2026-06-01 20:07:30 -0700

    Eliminar archivos temporales del repositorio

--- Ecommerce.DTO/obj/Debug/net10.0/Ecommerce.DTO.AssemblyInfo.cs
+++ Ecommerce.DTO/obj/Debug/net10.0/Ecommerce.DTO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.DTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff659968579fb84aae505248e4bfb528fa5962a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.DTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.DTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Ecommerce.WebAssembly/obj/Debug/net10.0/Ecommerce.WebAssembly.AssemblyInfo.cs
+++ Ecommerce.WebAssembly/obj/Debug/net10.0/Ecommerce.WebAssembly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.WebAssembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff659968579fb84aae505248e4bfb528fa5962a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.WebAssembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.WebAssembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
